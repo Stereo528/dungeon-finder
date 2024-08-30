f654bd07030d9040eaf3da93544673d94bc7d3bd
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2024-08-29 18:28:39 -0600

    make the command work and add configurable size

--- src/main/java/dev/stereo528/dungeon_finder/LocateDungeonCommand.java
+++ src/main/java/dev/stereo528/dungeon_finder/LocateDungeonCommand.java
@@ -3,6 +3,7 @@ package dev.stereo528.dungeon_finder;
 import com.google.common.base.Stopwatch;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import net.minecraft.ChatFormatting;
@@ -30,43 +31,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LocateDungeonCommand {
-    private static final DynamicCommandExceptionType ERROR_NO_SPAWNER_FOUND = new DynamicCommandExceptionType((object) -> Component.literal("FUCK"));
+    private static final DynamicCommandExceptionType ERROR_NO_SPAWNER_FOUND = new DynamicCommandExceptionType((object) -> Component.translatable("dungeon_finder.no_spawner_found"));
 
     public LocateDungeonCommand() {}
 
     public static void register(CommandDispatcher<CommandSourceStack> commandDispatcher) {
         commandDispatcher.register(Commands.literal("locate_spawner")
                 .requires((commandSourceStack) -> commandSourceStack.hasPermission(2))
-                .executes((context -> locateSpawner(context.getSource(), ResourceOrTagArgument.getResourceOrTag(context, "spawner", Registries.BLOCK))))
+                        .then(Commands.literal("list").executes((context -> locateSpawner(context.getSource(), true, 32)))
+                                .then(Commands.literal("size").then(Commands.argument("size", IntegerArgumentType.integer(1, 64)).executes(context -> locateSpawner(context.getSource(), true, IntegerArgumentType.getInteger(context, "size"))))))
+                        .then(Commands.literal("size").then(Commands.argument("size", IntegerArgumentType.integer(1, 64)).executes(context -> locateSpawner(context.getSource(), false, IntegerArgumentType.getInteger(context, "size")))))
+                .executes((context -> locateSpawner(context.getSource(), false, 32)))
         );
     }
 
-    private static int locateSpawner(CommandSourceStack commandSourceStack, ResourceOrTagArgument.Result<Block> result) throws CommandSyntaxException {
+    private static int locateSpawner(CommandSourceStack commandSourceStack, boolean printList, int size) throws CommandSyntaxException {
         BlockPos blockPos = BlockPos.containing(commandSourceStack.getPosition());
-        System.out.println(blockPos);
         ServerLevel serverLevel = commandSourceStack.getLevel();
         Stopwatch stopwatch = Stopwatch.createStarted(Util.TICKER);
-        List<BlockPos> blockPosList = getBlockPosList(blockPos, serverLevel);
-        System.out.println(blockPosList);
+        List<BlockPos> blockPosList = getBlockPosList(blockPos, serverLevel, (double) size);
         stopwatch.stop();
         if (blockPosList.isEmpty()) {
-            throw ERROR_NO_SPAWNER_FOUND.create(result.asPrintable());
+            throw ERROR_NO_SPAWNER_FOUND.create("");
         }
         else {
-            return locateSuccess(commandSourceStack, result, blockPos, blockPosList, stopwatch.elapsed());
+            return locateSuccess(commandSourceStack, blockPos, blockPosList, printList, stopwatch.elapsed());
         }
 
     }
 
-    private static int locateSuccess(CommandSourceStack commandSourceStack, ResourceOrTagArgument.Result<Block> result, BlockPos blockPos, List<BlockPos> blockPosList, Duration duration) {
+    private static int locateSuccess(CommandSourceStack commandSourceStack , BlockPos blockPos, List<BlockPos> blockPosList, boolean printList, Duration duration) {
         BlockPos closest = blockPosList.getFirst();
-        int distance = Mth.floor(Mth.sqrt((float) blockPos.distSqr(closest)));
-        String blockPosListString = blockPosList.toString();
-        Component component = ComponentUtils.wrapInSquareBrackets(Component.literal(closest.getX() + " " + closest.getY() + " " + closest.getZ()))
+        for (BlockPos blockPos0 : blockPosList) {
+            int dist = Mth.floor(Mth.sqrt((float)blockPos.distSqr(blockPos0)));
+            int closestDist = Mth.floor(Mth.sqrt((float)blockPos.distSqr(closest)));
+            if (dist < closestDist) {
+                closest = blockPos0;
+            }
+        }
+        BlockPos finalClosest = closest;
+        int distance = Mth.floor(Mth.sqrt((float) blockPos.distSqr(finalClosest)));
+        String distanceStr = "(" + distance + " Blocks Away)";
+        StringBuilder blockPosListString = new StringBuilder();
+        if(printList) {
+            for (BlockPos blockPos1 : blockPosList) {
+                blockPosListString.append("\n").append("[").append(blockPos1.getX()).append(", ").append(blockPos1.getY()).append(", ").append(blockPos1.getZ()).append("]");
+            }
+        }
+
+        Component component = ComponentUtils.wrapInSquareBrackets(Component.literal(finalClosest.getX() + " " + finalClosest.getY() + " " + finalClosest.getZ()))
                 .withStyle(style ->
-                        style.withColor(ChatFormatting.GREEN).withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/tp @s " + closest.getX() + " " + closest.getY() + " " + closest.getZ())).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("chat.coordinates.tooltip")))
+                        style.withColor(ChatFormatting.GREEN).withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/tp @s " + finalClosest.getX() + " " + finalClosest.getY() + " " + finalClosest.getZ())).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("chat.coordinates.tooltip")))
                 );
-        commandSourceStack.sendSuccess(() -> Component.translatable("dungeon_finder.found_dungeon.success", "spawner", component, distance, blockPosListString), false);
+        String string = "dungeon_finder.found_dungeon.success";
+        commandSourceStack.sendSuccess(() -> Component.translatable(string, component, distanceStr, blockPosListString.toString()), false);
         return Command.SINGLE_SUCCESS;
     }
 
@@ -82,8 +100,8 @@ public class LocateDungeonCommand {
         return blocks;
     }
 
-    private static List<BlockPos> getBlockPosList(BlockPos blockPos, Level level) {
-        AABB box = new AABB(blockPos).inflate(25);
+    private static List<BlockPos> getBlockPosList(BlockPos blockPos, Level level, double size) {
+        AABB box = new AABB(blockPos).inflate(size, size, size);
         List<BlockPos> blockPosList = new ArrayList<>();
         for(BlockPos blockPos1 : getBlockPosInAABB(box)) {
             BlockEntity blockEntity = level.getBlockEntity(blockPos1);
